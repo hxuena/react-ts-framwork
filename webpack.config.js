417b6d40426cff63883b2d75d33043d4f59cee05
xnhao <xnhao@iflytek.com>
2019-07-05 07:38:59 +0800

    modify

--- webpack.config.js
+++ webpack.config.js
@@ -3,23 +3,30 @@ const _mode = argv.mode || "development"
 const _mergeConfig = require(`./config/webpack.${_mode}.js`)
 const merge = require('webpack-merge')
 const {join, resolve} = require("path")
+const { CheckerPlugin } = require('awesome-typescript-loader')
 
 
 const webpackConfig = {
   entry: {
     app: resolve(__dirname, "./src/web/index.tsx")
   },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
   module: {
     rules: [
       {
-        test: /.(js|jsx|ts|tsx)$/,
+        test: /\.(js|jsx|ts|tsx)?$/,
         include: [ resolve('src') ],
         exclude: /node_modules/,
-        loader: ""
+        loader: "awesome-typescript-loader"
   
       }
-    ]
-  }
+    ],
+  },
+  plugins: [
+    new CheckerPlugin()
+  ]
   
 }
 
